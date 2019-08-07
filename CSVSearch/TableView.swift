c0b6d40f1dabc742698bdc7d7228122a621b85b7
Mark Doyle <mdoyle1@gmail.com>
2019-08-06 22:25:40 -0400

    TableView Class

--- CSVSearch/TableView.swift
+++ CSVSearch/TableView.swift
@@ -9,16 +9,50 @@
 import Foundation
 import Cocoa
 
-//class TableView:NSObject{
-//
-//    
-//   
-//   
-//
-//
-//  
-//    
-//}
-//
-//extension TableView: NSTableViewDelegate, NSTableViewDataSource {
-//   
+
+
+class TableView: NSTableView, NSTableViewDataSource, NSTableViewDelegate {
+    
+    var tableContainer = NSScrollView.init(frame: NSRect(x:0, y: 0, width: 800, height: 200))
+    var tableView:NSTableView = NSTableView(frame: NSRect(x:0, y: 0, width: 800, height: 200))
+    
+    func populateHeaders(){
+        for x in 0..<headers.count {
+            let column = NSTableColumn.init(identifier:NSUserInterfaceItemIdentifier(rawValue: headers[x]))
+            tableView.addTableColumn(column)
+            column.headerCell = NSTableHeaderCell(textCell: headers[x])
+        }
+    }
+    
+    func setup (){
+        tableView.usesAlternatingRowBackgroundColors = true
+        tableView.reloadData()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableContainer.documentView = tableView
+        tableContainer.hasVerticalScroller = true
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return csvArray.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        let dict = csvArray[row]
+        for x in 0..<headers.count {
+            if ((tableColumn?.identifier)!.rawValue == headers[x]) {
+                return dict[headers[x]]
+            }
+        }
+        return "nil"
+        
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return true
+    }
+
+   
+}
+
+
--- CSVSearch/ViewController.swift
+++ CSVSearch/ViewController.swift
@@ -12,37 +12,18 @@ var csvArray = [[String:String]]()
 var headers = [String]()
 var globalPathToCsv:NSURL!
 var csv = CSVaccessability()
-
-class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
-    var tableContainer = NSScrollView.init(frame: NSRect(x:0, y: 0, width: 800, height: 200))
-    var tableView:NSTableView = NSTableView(frame: NSRect(x:0, y: 0, width: 800, height: 200))
-    func populateHeaders(){
-        for x in 0..<headers.count {
-            let column = NSTableColumn.init(identifier:NSUserInterfaceItemIdentifier(rawValue: headers[x]))
-            tableView.addTableColumn(column)
-            column.headerCell = NSTableHeaderCell(textCell: headers[x])
-        }
-    }
-    
-    func setup (){
-        tableView.usesAlternatingRowBackgroundColors = true
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.reloadData()
-        tableContainer.documentView = tableView
-        tableContainer.hasVerticalScroller = true
-    }
-    
-
+var tableView1 = TableView()
+class ViewController: NSViewController{
+  
     @IBOutlet weak var submitBtnObj: NSButton!
     @IBOutlet weak var importBtnObj: NSButton!
     @IBAction func submitBtn(_ sender: NSButton) {
         importBtnObj.isHidden = false
         csv.csvToList()
-        self.view.addSubview(tableContainer)
         submitBtnObj.isHidden = true
-        setup()
-        populateHeaders()
+        tableView1.setup()
+        tableView1.populateHeaders()
+        self.view.addSubview(tableView1.tableContainer)
      
     }
     
@@ -73,25 +54,6 @@ importBtnObj.isHidden = true
         // Update the view, if already loaded.
         }
     }
-    func numberOfRows(in tableView: NSTableView) -> Int {
-        return csvArray.count
-    }
-    
-    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
-        let dict = csvArray[row]
-        for x in 0..<headers.count {
-            if ((tableColumn?.identifier)!.rawValue == headers[x]) {
-                return dict[headers[x]]
-                
-            }
-        }
-        return "nil"
-        
-    }
    
-    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
-        return true
-    }
-
 }
 
